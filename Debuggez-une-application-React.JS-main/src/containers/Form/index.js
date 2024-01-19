5851983e9fcdf1bd189dc5a13cc5764a9f74099e
Sonia Sabbahi <soniadevpro@gmail.com>
2024-01-19 23:15:18 +0100

    Update FORM setTimeout

--- Debuggez-une-application-React.JS-main/src/containers/Form/index.js
+++ Debuggez-une-application-React.JS-main/src/containers/Form/index.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
@@ -6,11 +6,12 @@ import Button, { BUTTON_TYPES } from "../../components/Button";
 
 const mockContactApi = () =>
   new Promise((resolve) => {
-    setTimeout(resolve, 1000);
+    setTimeout(resolve, 900);
   });
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
+
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
@@ -18,7 +19,8 @@ const Form = ({ onSuccess, onError }) => {
       // We try to call mockContactApi
       try {
         await mockContactApi();
-        onSuccess(false); // setSending
+        setSending(false);
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
@@ -26,20 +28,21 @@ const Form = ({ onSuccess, onError }) => {
     },
     [onSuccess, onError]
   );
+
   return (
     <form onSubmit={sendContact}>
       <div className="row">
         <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
+          <Field placeholder="Votre nom" label="Nom" />
+          <Field placeholder="Votre prénom" label="Prénom" />
           <Select selection={["Personel", "Entreprise"]} onChange={() => null} label="Personel / Entreprise" type="large" titleEmpty />
-          <Field placeholder="" label="Email" />
+          <Field placeholder="Entrez votre email" label="Email" type={FIELD_TYPES.EMAIL} />
           <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
             {sending ? "En cours" : "Envoyer"}
           </Button>
         </div>
         <div className="col">
-          <Field placeholder="message" label="Message" type={FIELD_TYPES.TEXTAREA} />
+          <Field placeholder="Votre message" label="Message" type={FIELD_TYPES.TEXTAREA} />
         </div>
       </div>
     </form>
