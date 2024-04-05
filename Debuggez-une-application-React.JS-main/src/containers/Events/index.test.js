05b4f8677e6f74e68f24ac229a7a728abd4964c7
Sonia Sabbahi <soniadevpro@gmail.com>
2024-04-05 16:34:08 +0200

    Update index.test.js

--- Debuggez-une-application-React.JS-main/src/containers/Events/index.test.js
+++ Debuggez-une-application-React.JS-main/src/containers/Events/index.test.js
@@ -10,16 +10,10 @@ const data = {
       date: "2022-04-29T20:28:45.744Z",
       title: "Conférence #productCON",
       cover: "/images/stem-list-EVgsAbL51Rk-unsplash.png",
-      description:
-        "Présentation des outils analytics aux professionnels du secteur",
+      description: "Présentation des outils analytics aux professionnels du secteur",
       nb_guesses: 1300,
       periode: "24-25-26 Février",
-      prestations: [
-        "1 espace d’exposition",
-        "1 scéne principale",
-        "2 espaces de restaurations",
-        "1 site web dédié",
-      ],
+      prestations: ["1 espace d’exposition", "1 scéne principale", "2 espaces de restaurations", "1 site web dédié"],
     },
 
     {
@@ -28,8 +22,7 @@ const data = {
       date: "2022-04-29T20:28:45.744Z",
       title: "Forum #productCON",
       cover: "/images/stem-list-EVgsAbL51Rk-unsplash.png",
-      description:
-        "Présentation des outils analytics aux professionnels du secteur",
+      description: "Présentation des outils analytics aux professionnels du secteur",
       nb_guesses: 1300,
       periode: "24-25-26 Février",
       prestations: ["1 espace d’exposition", "1 scéne principale"],
